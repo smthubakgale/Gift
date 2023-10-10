d5ffd8d64e769c68a2a4923ca582b9bc8221fa6f
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2023-10-10 13:11:35 +0200

    Update 2_buzzer_1.py

--- 2_buzzer_1.py
+++ 2_buzzer_1.py
@@ -131,8 +131,8 @@ beat_1 = [
 def buzz_sound(song , be1t):
   while True:
      for i in range(1, len(song)):
-	     Buzz.ChangeFrequency(song[i])
-	     time.sleep(beat[i]*0.13) 
+        Buzz.ChangeFrequency(song[i])
+        time.sleep(beat[i]*0.13) 
   pass
   
 def distance():
