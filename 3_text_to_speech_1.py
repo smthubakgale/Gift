a478970d4394ca014120bb1d71192d6a2890967f
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2023-10-11 11:41:29 +0200

    Create 3_text_to_speech_1.py

---
+++ 3_text_to_speech_1.py
@@ -0,0 +1,111 @@
+# Author : Thubakgale Mabalane 
+# Date : 11/10/2023
+#::: Ultrasonic + Buzzer + Text-To-Speech  Example 
+#----------------------------- :: Modules
+import RPi .GPIO as GPIO 
+import time 
+import pyaudio 
+
+#----------------------------- :: PINS
+#:": Ultrasonic 
+GPIO_TRIGGER = 18              # trigger
+GPIO_ECHO = 24                 # Echo 
+
+#::: Buzzer
+BuzzerPin = 4
+
+#::: Text-To-Speech
+audio = pyaudio.PyAudio();
+
+#----------------------------- :: Variables
+
+
+#----------------------------- :: Methods 
+def buzz_sound(song , beat):
+  
+  Buzz = GPIO.PWM(BuzzerPin, 440)
+  Buzz.start(60) 
+   
+  for i in range(1, len(song)):
+     if song[i] != 0 :
+        Buzz.start(60) 
+        Buzz.ChangeFrequency(song[i])
+     else:
+          Buzz.stop()
+     time.sleep(beat[i]*0.13) 
+
+  Buzz.stop()
+  pass
+def distance():
+  
+  #--- send trigger for 0.01 ms
+  GPIO.output(GPIO_TRIGGER, True) 
+  time.sleep(0.00001)
+  GPIO.output(GPIO_TRIGGER, False)
+
+  #--- measure echo duration 
+  StartTime = time.time()
+  StopTime = time.time()
+ 
+  #:: save StartTime
+  while GPIO.input(GPIO_ECHO) == 0:
+     StartTime = time.time()
+ 
+  #:: save time of arrival
+  while GPIO.input(GPIO_ECHO) == 1:
+     StopTime = time.time()
+ 
+  #:: time difference between start and arrival : divide by 2 
+  TimeElapsed = (StopTime - StartTime)/2
+  
+  # total distance = total time * sonic speed (34300 cm/s))
+  distance = (TimeElapsed * 34300) 
+  #---
+  
+  return distance 
+  pass
+# ---------------------------- :: Setup
+def my_setup():
+
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setwarnings(False)
+  
+  GPIO.setup(GPIO_TRIGGER, GPIO.OUT)   
+  GPIO.setup(GPIO_ECHO , GPIO.IN)   
+  
+  GPIO.setup(BuzzerPin, GPIO.OUT) 
+   
+  print("Set Up")
+  pass
+#------------------------------:: Loop
+def my_loop():
+  
+  # Buzzer
+  b1 = [ 5 , 5 , 5 , 5 , 5 , 5  ] # time
+  f1 = [ 0 , 1 , 0 , 1 , 0 , 1  ] # frequency 
+  
+  buzz_sound(f1 , b1);
+  # Ultrasonic
+  dist = distance()
+
+  print(str(dist))
+  # Text to Speech
+  for k in range(0 , audio.get_device_count()):
+    print(audio.get_device_info_by_index(k));
+  #
+  print("Loop after 10 second")
+  time.sleep(10)
+  
+  pass 
+#------------------------------:: Main 
+# Main function
+def main () :
+# Setup 
+ my_setup()
+# Infinite loop
+ while 1 : 
+  my_loop()
+  pass
+# Command line execution
+if __name__ == '__main__' :
+   main()
