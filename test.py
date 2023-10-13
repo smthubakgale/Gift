f1a3c392fb6bd1a2d8c29fd6328ac1157f809c2b
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2023-10-13 12:26:13 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+import face_recognition
+known_image = face_recognition.load_image_file("0001.jpg")
+unknown_image = face_recognition.load_image_file("0002.jpg")
+
+biden_encoding = face_recognition.face_encodings(known_image)[0]
+unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+
+results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
+
+print(results)
