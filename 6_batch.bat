2f96f162195f899fce45df80fa2309139ed07709
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2023-10-16 13:39:35 +0200

    Create 6_batch.bat
    Required Libraries to install on the pi

---
+++ 6_batch.bat
@@ -0,0 +1,9 @@
+
+rem 1. Libraries 
+
+pip install --force-reinstall -v "dlib==19.24.0"
+pip install --force-reinstall -v "face-recognition==1.3.0"
+pip install --force-reinstall -v "numpy==1.24.2"
+pip install --force-reinstall -v "Pillow==9.4.0"
+
+
