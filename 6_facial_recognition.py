3d2fcd2554e211a7f2d682d533c72c79e5ec7195
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2023-10-16 13:48:19 +0200

    Create 6_facial_recognition.py
    Facial Recogtinion Algorithm

---
+++ 6_facial_recognition.py
@@ -0,0 +1,37 @@
+# Author : Thubakgale Mabalane 
+# Date : 16/10/2023
+#::: Facial Recognition Example 
+#----------------------------- :: Modules
+#:: detector libraries
+from pathlib import Path
+import face_recognition
+# 
+
+
+#----------------------------- :: Methods 
+def face_recognize():
+  pass
+
+# ---------------------------- :: Setup
+def my_setup():
+  print("Set Up")
+  pass
+#------------------------------:: Loop
+def my_loop():
+  print("Loop after 10 second")
+  time.sleep(10)
+  
+  pass 
+#------------------------------:: Main 
+# Main function
+def main () :
+# Setup 
+ my_setup()
+# Infinite loop
+ while 1 : 
+  my_loop()
+  pass
+# Command line execution
+if __name__ == '__main__' :
+   main()
+
