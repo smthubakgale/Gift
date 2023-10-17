7de4a80453944dd73f7e18bc6509ee6e37d1723f
smthubakgale <97933623+smthubakgale@users.noreply.github.com>
2023-10-17 12:45:26 +0200

    Update 7_third_eye.py

--- 7_third_eye.py
+++ 7_third_eye.py
@@ -52,13 +52,14 @@ def obst():
   
   d = distance()
   
-  if d < 50 :
+  print(str(d))
+  #if d < 50 :
     #b1 = [ 5 , 5 , 5 , 5 , 5 , 5  ] # time
     #f1 = [ 0 , 1 , 0 , 1 , 0 , 1  ] # frequency 
   
     #buzz_sound(f1 , b1);
-    text = "collision imminent"
-    speak(text)
+    #text = "collision imminent"
+    #speak(text)
   if d < 150 :
     dist = round(d) 
     text = "object detected " + str(dist) + " centimetres away"
